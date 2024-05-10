e1645e0dfc21317cfd6a5aed98a1f4ca2906c391
Lima <jvbarb8@gmail.com>
2024-05-10 19:11:50 -0300

    Python-basico

---
+++ tipo_de_Dados.py
@@ -0,0 +1,30 @@
+#anotaçoes
+
+print(10 + 10) #numeros inteiro "int"
+print(1.50 + 3 + 0,5) #numero de ponto flutuante "float"
+print(True) #booleano
+print(False) #booleano
+print("python") #Strings "str"
+
+int()
+float()
+str()
+bool()
+
+
+ABS_PATH = 'SEU CAMINHO' # CONSTANTE
+DEBUG = True # CONSTANTE
+STATES = [
+    'SP',
+    'RJ',  # CONSTANTE
+    'MG',
+]
+AMOUNT = 30.2 # CONSTANTE
+
+
+# Python nao tem constantes, Em python usamos a convençao que diz ao programador que a variavel e uma constante. para fazer isso, vode deve criar a variavel com o nome em letras maíuscula:
+# Boas práticas
+# > O padrao de nomes deve ser snake case ----> Exemplo: '''Valor_Total'''
+# > Escolher nomes sugestivos ----> ao inves de usar "A" usar "VALOR_TOTAL1"
+# > Nome de constante todo em maiúsculo
+
---
+++ variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = "Joao Victor"
+idade = "18"
+
+nome2, idade2 = "anne", 23
+
+
+print(nome, idade)
+print(nome2, idade2)
+
+limite_saque_diario = 1000
+
+BRAZILIAM_STATES = ["SP", "RJ", "SC", "RS"]
